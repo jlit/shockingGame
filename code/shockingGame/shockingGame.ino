cdf4f230b606209acbe393a1a827c2c24ccfc2fc
jlit <jeff.littrell@caa.com>
2026-01-19 21:54:43 -0800

    Remove twinkle, add random mode, add shocking relay, tweak CAD to remove holiday message, drop start button mounts, new mode label

--- code/shockingGame/shockingGame.ino
+++ code/shockingGame/shockingGame.ino
@@ -6,11 +6,7 @@
 
 // LED stuff
 #define NUM_LEDS 20
-#define TWINKLE_SPEED 6
-#define TWINKLE_DENSITY 6
 CRGBArray<NUM_LEDS> leds;
-CRGBPalette16 gCurrentPalette = *&RainbowColors_p;
-CRGB gBackgroundColor = CRGB::Black;
 int LEDSequenceNumber = -1; // tracks where in an LED sequence we are so that it is non-blocking
 
 // Global play options
@@ -21,12 +17,13 @@ const int maxDelayBetweenResponses = 5000;
 const int debounceTime = 50;
 
 // Pins
-const int buttonStartPin = 3;
+const int buttonStartPin = 7;
 const int buttonNumberOfPlayersPin = 12;
-const int LEDPin = A0;
-const int modePins[3] = {9, 10, 11}; // Reaction, Roulette, Recall
-const int buttonPins[MAX_PLAYERS] = {4, 5, 6, 7, 8};
-const int shockPins[MAX_PLAYERS] = {A5, A4, A3, A2, A1};
+const int LEDPin = 19;
+const int shockRelayPin = A5;
+const int modePins[4] = {8, 9, 10, 11}; // Reaction, Recall, Roulette, Random
+const int buttonPins[MAX_PLAYERS] = {2, 3, 4, 5, 6};
+const int shockPins[MAX_PLAYERS] = {A4, A3, A2, A1, A0};
 const int buzzer = 13; 
 
 enum State {
@@ -38,8 +35,9 @@ State currentState = WAITING;
 
 enum Mode {
   REACTION,
+  RECALL,
   ROULETTE,
-  RECALL
+  RANDOM
 };
 Mode currentMode = RECALL;
 
@@ -87,13 +85,13 @@ bool settingsChanged() {
     changed = true;
     delay(50); // to prevent bounce
   }
-  for(int pin = 0; pin < 3; pin++) {
+  for(int pin = 0; pin < 4; pin++) {
     if(digitalRead(modePins[pin]) == LOW) {
       if(pin != static_cast<int>(currentMode)) {
         Serial.print(F("game mode changed to "));
         Serial.println(pin);
         changed = true;
-        currentMode = static_cast<Mode>(pin); // 0 = Reaction, 1 = Roulette, 2 = recall
+        currentMode = static_cast<Mode>(pin); // 0 = Reaction, 1 = Recall, 2 = Roulette, 3 = Random
       }
     }
   }
@@ -319,9 +317,11 @@ void shockCheater(int playerNum) {
   noTone(buzzer);
   LEDsPlayerOneColor(playerNum, CRGB::Red);
 
+  digitalWrite (shockRelayPin, HIGH);
   digitalWrite (shockPin, HIGH);
   delay(1000); // cheaters get a full second
   digitalWrite (shockPin, LOW);
+  digitalWrite (shockRelayPin, LOW);
 
   playBoom();
   LEDsPlayerFlashOneColor(playerNum, CRGB::Red);
@@ -335,12 +335,14 @@ void shockPlayer(int playerNum) {
   Serial.print(F("Shocking player "));
   Serial.println(playerNum);
 
-  noTone(buzzer);  
+  noTone(buzzer);
+  digitalWrite (shockRelayPin, HIGH);
   digitalWrite (shockPin, HIGH);
   playFail();
   LEDsPlayerOneColor(playerNum, CRGB::Red);
   delay(shockTime);
   digitalWrite (shockPin, LOW);
+  digitalWrite (shockRelayPin, LOW);
   LEDsPlayerFlashOneColor(playerNum, CRGB::Red);
 }
 // ============================================================================
@@ -439,80 +441,17 @@ void LEDsAllRadial(CRGB color) {
   delay(150);
 }
 
-//  This function loops over each pixel, calculates the adjusted 'clock' that this pixel should use, and calls 
-//  "CalculateOneTwinkle" on each pixel.  It then displays either the twinkle color of the background color, whichever is brighter.
-void LEDsTwinkle( CRGBSet& L) {
-  uint16_t PRNG16 = 11337; 
-  uint32_t clock32 = millis();
- 
-  CRGB bg = gBackgroundColor;
-  uint8_t backgroundBrightness = bg.getAverageLight();
-  
-  for(CRGB& pixel: L) {
-    PRNG16 = (uint16_t)(PRNG16 * 2053) + 1384; // next 'random' number
-    uint16_t myclockoffset16= PRNG16; // use that number as clock offset
-    PRNG16 = (uint16_t)(PRNG16 * 2053) + 1384; // next 'random' number
-    // use that number as clock speed adjustment factor (in 8ths, from 8/8ths to 23/8ths)
-    uint8_t myspeedmultiplierQ5_3 =  ((((PRNG16 & 0xFF)>>4) + (PRNG16 & 0x0F)) & 0x0F) + 0x08;
-    uint32_t myclock30 = (uint32_t)((clock32 * myspeedmultiplierQ5_3) >> 3) + myclockoffset16;
-    uint8_t  myunique8 = PRNG16 >> 8; // get 'salt' value for this pixel
- 
-    // We now have the adjusted 'clock' for this pixel, now we call the function that computes what color the pixel should be based
-    // on the "brightness = f( time )" idea.
-    CRGB c = computeOneTwinkle( myclock30, myunique8);
- 
-    uint8_t cbright = c.getAverageLight();
-    int16_t deltabright = cbright - backgroundBrightness;
-    if( deltabright >= 32 || (!bg)) {
-      // If the new pixel is significantly brighter than the background color, use the new color.
-      pixel = c;
-    } else if( deltabright > 0 ) {
-      // If the new pixel is just slightly brighter than the background color, mix a blend of the new color and the background color
-      pixel = blend( bg, c, deltabright * 8);
-    } else { 
-      // if the new pixel is not at all brighter than the background color, just use the background color.
-      pixel = bg;
-    }
+void LEDsRandomPlayer(CRGB color) {  
+  int player = random(numberOfPlayers);
+  if(color == CRGB::Black) { // If we passed in black then use the Recall game color for each player
+    color = playerColors[player];
   }
-
+  FastLED.clear(true);
+  for(int led = 0; led < numberOfLEDsPerPlayer; led++) {
+    leds[player*numberOfLEDsPerPlayer+led] = color;
+  }
   FastLED.show();
-}
-
-//  This function takes a time in pseudo-milliseconds, figures out brightness = f( time ), and also hue = f( time )
-//  The 'low digits' of the millisecond time are used as input to the brightness wave function.  
-//  The 'high digits' are used to select a color, so that the color does not change over the course of the fade-in, fade-out
-//  of one cycle of the brightness wave function.
-//  The 'high digits' are also used to determine whether this pixel should light at all during this cycle, based on the TWINKLE_DENSITY.
-CRGB computeOneTwinkle( uint32_t ms, uint8_t salt) {
-  uint16_t ticks = ms >> (8-TWINKLE_SPEED);
-  uint8_t fastcycle8 = ticks;
-  uint16_t slowcycle16 = (ticks >> 8) + salt;
-  slowcycle16 += sin8( slowcycle16);
-  slowcycle16 =  (slowcycle16 * 2053) + 1384;
-  uint8_t slowcycle8 = (slowcycle16 & 0xFF) + (slowcycle16 >> 8);
-  
-  uint8_t bright = 0;
-  if( ((slowcycle8 & 0x0E)/2) < TWINKLE_DENSITY) {
-    bright = attackDecayWave8( fastcycle8);
-  }
- 
-  uint8_t hue = slowcycle8 - salt;
-  CRGB c;
-  if( bright > 0) {
-    c = ColorFromPalette( gCurrentPalette, hue, bright, NOBLEND);
-  } else {
-    c = CRGB::Black;
-  }
-  return c;
-}
- 
-uint8_t attackDecayWave8( uint8_t i) {
-  if( i < 86) {
-    return i * 3;
-  } else {
-    i -= 86;
-    return 255 - (i + (i/2));
-  }
+  delay(150);
 }
 // ============================================================================
 
@@ -578,14 +517,15 @@ void setup() {
   FastLED.addLeds<WS2812, LEDPin, GRB>(leds, NUM_LEDS);
   pinMode(buttonStartPin, INPUT_PULLUP);
   pinMode(buzzer, OUTPUT);
-  for(int modePin = 0 ; modePin < sizeof(modePins); modePin++) {
+  for(int modePin = 0 ; modePin < 4; modePin++) {
     pinMode(modePins[modePin], INPUT_PULLUP);
   }
   pinMode(buttonNumberOfPlayersPin, INPUT_PULLUP);
-  for(int buttonPin = 0 ; buttonPin < sizeof(buttonPins); buttonPin++) {
+  for(int buttonPin = 0 ; buttonPin < MAX_PLAYERS; buttonPin++) {
     pinMode(buttonPins[buttonPin], INPUT_PULLUP);
   }
-  for(int shockPin = 0 ; shockPin < sizeof(shockPins); shockPin++) {
+  pinMode(shockRelayPin, OUTPUT);
+  for(int shockPin = 0 ; shockPin < MAX_PLAYERS; shockPin++) {
     pinMode(shockPins[shockPin], OUTPUT);
   }
   LEDsTest(CRGB::Green);
@@ -601,14 +541,17 @@ void loop() {
   }
 
   while (digitalRead(buttonStartPin) == HIGH) {
-    if(currentMode == RECALL) {
-      LEDsTwinkle(leds);
-    }
     if(currentMode == REACTION) {
       LEDsAllSequential(CRGB::Green);
     }
+    if(currentMode == RECALL) {
+      LEDsRandomPlayer(CRGB::Black);  // Actually uses playeColors
+    }
     if(currentMode == ROULETTE) {      
       LEDsAllRadial(CRGB::Blue);
+    }
+    if(currentMode == RANDOM) {
+      LEDsRandomPlayer(CRGB::Cyan);
     }
     if(settingsChanged()) {
       reset();
@@ -616,6 +559,27 @@ void loop() {
   }
 
   currentState = PLAYING;
+
+  if(currentMode == RANDOM) {
+    Serial.println (F("Starting Random mode..."));
+    LEDsAllOneColor(CRGB::White);
+    
+    FastLED.clear(true);
+    for (int i = 20; i < 255; i++) { // GAME START BUZZER SOUND
+      tone(buzzer, i * 7);
+      delay(15);
+    }
+    noTone(buzzer);
+    FastLED.clear(true);
+
+    delay(random(2000, 6000));  // Wait 2-6 seconds
+    Serial.println(F("Random wait over"));
+
+    if(currentState == PLAYING) {
+      shockPlayer(random(numberOfPlayers));
+    }
+    reset();
+  }
 
   if(currentMode == ROULETTE) {
     Serial.println (F("Starting Russian roulette mode..."));
